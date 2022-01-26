bfa58e0e265cfa918b71190f11d9cbb5eb7fe3d7
Anaïs Rosa <anaisrosa.lppgmail.com>
2022-01-26 12:11:07 +0100

    fetch all posts

--- groupomania/src/views/Feed.vue
+++ groupomania/src/views/Feed.vue
@@ -4,13 +4,11 @@
     <div>
       <h1>Feed des posts</h1>
 
-      <section class="items" id="items">
-        
-          <article>
-              <h3>{{}}</h3>
-            </article>
-        
-      </section>
+      <div v-for="post in posts" v-bind:key="post.id">
+      <h3>{{ post.title }}</h3>
+      <p>{{ post.content }}</p><br>
+    </div>
+
     </div>
   </div>
 </template>
@@ -20,5 +18,34 @@
 
 export default {
   name: "Feed",
-};
+  data() {
+    let posts = [];
+    return {
+      posts,
+    };
+  },
+  methods: {
+    async getData() {
+      try {
+        let response = await fetch("http://localhost:3000/api/posts");
+        this.posts = await response.json();
+        console.table(this.posts);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    
+  },
+  created () {
+    this.getData();
+},
+
+}
 </script>
+
+<style scoped>
+a {
+  text-decoration: none;
+  color: #2c3e50;
+}
+</style>
